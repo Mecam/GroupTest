6ac1a33985be0862a68e7d3bda67e8fd4aaa26d7
Conner Tenn <connertenn@gmail.com>
2015-08-26 14:13:54 -0400

    UP

--- GroupTest/GroupTest/GroupTest.cpp
+++ GroupTest/GroupTest/GroupTest.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <C:/Users/Conner/Desktop/Programming/Visual Studio/Libraries/GLlibs/GL/glew.h>
 using namespace std;
 
 void main()
