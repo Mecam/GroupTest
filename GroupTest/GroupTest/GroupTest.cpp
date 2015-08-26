ee6a5f4063a4e5eda28bacb20f0ee51ed37b4ce6
Conner Tenn <connertenn@gmail.com>
2015-08-26 13:26:43 -0400

    Simple Setup

---
+++ GroupTest/GroupTest/GroupTest.cpp
@@ -0,0 +1,15 @@
+// GroupTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void main()
+{
+	cout << "Hello Project\n";
+	int a;
+	cin >> a;
+}
+
